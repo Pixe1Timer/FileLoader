4bb573cd3cfdc602ec46002f134fbb1d1445c5e7
ubuntu <denis.ksens@gmail.com>
2023-04-10 12:20:53 +0300

    added dir parsing and validation PRAC-13

--- classes/config_items.py
+++ classes/config_items.py
@@ -1,4 +1,5 @@
 from abc import ABC
+import re
 
 
 class BaseItem(ABC):
@@ -19,30 +20,31 @@ class BaseItem(ABC):
             if not type(line) is str:
                 raise TypeError('This parameter should be string!')
             elif len(line) > 19:
-                raise IndexError('Length is out of range!')
+                raise ValueError('Length is out of range!')
         if not data_tuple[0].islower() or not data_tuple[2].islower():
             raise ValueError('Values are not lowercase!')
 
     def is_valid_dict(self, data_dict):
         for key in data_dict:
-            if key != 'Path':
+            if key != 'path':
                 if not type(data_dict[key]) is str:
                     raise TypeError('This parameter should be string!')
-                if len(data_dict[key]) > 19:
-                    raise IndexError('Length is out of range!')
-                if (key != 'Event_handler') and not data_dict[key].islower():
+                if len(data_dict[key]) > 10000:
+                    raise ValueError('Length is out of range!')
+                if (key != 'event_handler') and not data_dict[key].islower():
                     raise ValueError('Values are not lowercase!')
 
 
 class Type1BaseItem(BaseItem):
-    Process_ID: str
-    Event_handler: str
-    Message_ID_suffix_2_letters: str
+    process_id: str
+    event_handler: str
+    message_id_suffix_2_letters: str
     din: str
     dout: str
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.path_list = []
 
     def is_valid_tuple(self, data_tuple):
         super().is_valid_tuple(data_tuple)
@@ -52,18 +54,26 @@ class Type1BaseItem(BaseItem):
         super().is_valid_dict(data_dict)
         pass
 
+    def split_path(self, path):
+        splitted_path = path.split(';')
+        if not re.match(r'DIR.IN=', splitted_path[0]) or not re.match(r'DIR.OUT=', splitted_path[1]):
+            raise ValueError('Невозможно найти нужные параметры!')
+        self.din = splitted_path[0].split('=')[1]
+        self.dout = splitted_path[1].split('=')[1]
+        return self.din, self.dout
+
     def args_parse(self, values):
         self.is_valid_tuple(values)
-        self.Process_ID = values[0]
-        self.Event_handler = values[1]
-        self.Message_ID_suffix_2_letters = values[2]
-        self.din = values[3].split(";")[0]
-        self.dout = values[3].split(";")[1]
+        self.process_id = values[0]
+        self.event_handler = values[1]
+        self.message_id_suffix_2_letters = values[2]
+        self.path_list = values[3]
+        self.split_path(self.path_list)
 
     def kwargs_parse(self, dict_values):
         self.is_valid_dict(dict_values)
-        self.Process_ID = dict_values.get('Process_ID')
-        self.Event_handler = dict_values.get('Event_handler')
-        self.Message_ID_suffix_2_letters = dict_values.get('Message_ID_suffix_2_letters')
-        self.din = dict_values['Path'].split(";")[0]
-        self.dout = dict_values['Path'].split(";")[1]
+        self.process_id = dict_values.get('process_id')
+        self.event_handler = dict_values.get('event_handler')
+        self.message_id_suffix_2_letters = dict_values.get('message_id_suffix_2_letters')
+        self.path_list = dict_values['path']
+        self.split_path(self.path_list)
--- classes/config_items_test.py
+++ classes/config_items_test.py
@@ -4,19 +4,19 @@ import unittest
 
 class Type1TestCase(unittest.TestCase):
     records = ('eraser', 'EraserHandler', 'er', 'DIR.IN=/usr/local/games/exch/proc/echo/in/;DIR.OUT=')
-    dict1 = {'Process_ID': 'switch', 'Event_handler': 'SwitchHandler', 'Message_ID_suffix_2_letters': 'sw', 'Path':
+    dict1 = {'process_id': 'switch', 'event_handler': 'SwitchHandler', 'message_id_suffix_2_letters': 'sw', 'path':
              'DIR.IN=/usr/local/games/exch/proc/echo/in/;DIR.OUT='}
 
     def test_tuple(self):
         t = Type1BaseItem(*self.records)
-        self.assertEqual(t.Process_ID, self.records[0])
-        self.assertEqual(t.Event_handler, self.records[1])
-        self.assertEqual(t.Message_ID_suffix_2_letters, self.records[2])
+        self.assertEqual(t.process_id, self.records[0])
+        self.assertEqual(t.event_handler, self.records[1])
+        self.assertEqual(t.message_id_suffix_2_letters, self.records[2])
         print(f'Input path:{t.din}, output path: {t.dout}')
 
     def test_dict(self):
         d = Type1BaseItem(**self.dict1)
-        self.assertEqual(d.Process_ID, self.dict1['Process_ID'])
-        self.assertEqual(d.Event_handler, self.dict1['Event_handler'])
-        self.assertEqual(d.Message_ID_suffix_2_letters, self.dict1['Message_ID_suffix_2_letters'])
+        self.assertEqual(d.process_id, self.dict1['process_id'])
+        self.assertEqual(d.event_handler, self.dict1['event_handler'])
+        self.assertEqual(d.message_id_suffix_2_letters, self.dict1['message_id_suffix_2_letters'])
         print(f'Input path:{d.din}, output path: {d.dout}')
