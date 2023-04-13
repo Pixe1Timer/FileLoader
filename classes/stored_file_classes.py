4150edbc76ae20354cd07b7a0d7b81ab247ec10e
Golitsyn <golizinpaul@gmail.com>
2023-04-13 17:41:22 +0300

    Законченный юнит тест (PRAC-14)

--- classes/stored_file_classes.py
+++ classes/stored_file_classes.py
@@ -23,31 +23,30 @@ class StoredFile:
         """
         return self.get_full_file_name
 
-    def get_full_file_name(self, file_title: str):
+    @property
+    def get_full_file_name(self):
         """
         Получить полный путь файла
         :return: str: объединение пути и имени файла
         """
-        self.file_name = file_title
         return os.path.join(self.file_path, self.file_name)
 
 
-class StoredFileContainer(StoredFile):
+class StoredFileContainer:
     """
     Класс, предназначенный для получения списка файлов и определения
     готовности файлов для обработки(отсутствие блокирововк на файле)
     """
-    def __init__(self, file_path: str, file_name: str):
-        super().__init__(file_path, file_name)
-
+    def __init__(self, file_directory: str):
         """
         Класс конструктор. Заполняет полными директориями фалов
         список для последующего использования класса
         :param str file_directory: путь файла без имени файла
         """
+        self.file_directory = file_directory
         self.files_list = []
-        for files_instance in os.listdir(file_path):
-            self.files_list.append(self.get_full_file_name(files_instance))
+        for files_instance in os.listdir(file_directory):
+            self.files_list.append(StoredFile(file_directory, files_instance))
 
     def get_unlocked_files(self) -> list[str]:
         """
@@ -57,6 +56,6 @@ class StoredFileContainer(StoredFile):
         unlocked_files_list = []
         block_files_det_obj = BlockedFilesDetector()
         for full_file_directory in self.files_list:
-            if not block_files_det_obj.file_is_locked(full_file_directory):
-                unlocked_files_list.append(full_file_directory)
+            if not block_files_det_obj.file_is_locked(full_file_directory.get_full_file_name):
+                unlocked_files_list.append(full_file_directory.get_full_file_name)
         return unlocked_files_list
