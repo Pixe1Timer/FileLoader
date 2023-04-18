69365e599dd2a354e06f89b7e4156c184b059db4
Golitsyn <golizinpaul@gmail.com>
2023-04-18 15:09:19 +0300

    На ревью (PRAC-25)

--- classes/bank_certificates_ classes.py
+++ classes/bank_certificates_ classes.py
@@ -1,15 +1,33 @@
-import os
-import typing
 from stored_file_classes import StoredFile, StoredFileContainer
+import datetime
+import typing
+import os
 
 
 class BankCertificateFile(StoredFile):
+
+    bank_code: str
+    snils: str
+    date_and_time: datetime.datetime
+    certificate_condition: str
 
     def __init__(self, file_path: str, file_name: str):
         super().__init__(file_path, file_name)
 
     def get_certificate_values(self):
-        super().get_certificate_values()
+        splited_file_name = self.file_name.split('.')
+        self.bank_code = splited_file_name[0]
+        self.snils = splited_file_name[1]
+        self.date_and_time = datetime.datetime.strptime(splited_file_name[2], '%Y%m%d%H%M%S')
+        self.certificate_condition = splited_file_name[3]
+#        print('Код банка: {}\nСНИЛС: {}\nСрок окончания сертификата: {}\nСостояние сертификата: {}'.
+#              format(self.bank_code, self.snils, self.date_and_time, self.certificate_condition))
+
+    def __str__(self):
+        super().__str__()
+
+    def get_full_file_name(self):
+        super().get_full_file_name()
 
 
 class BankCertificatesContainer(StoredFileContainer):
@@ -20,5 +38,17 @@ class BankCertificatesContainer(StoredFileContainer):
     def __init__(self, file_directory: str):
         super().__init__(file_directory)
 
-    def get_available_certificates(self) -> typing.Iterable[BankCertificateFile]:
-        pass
+    def get_available_certificates(self):
+        certificates_list = []
+        for certificate in self.files_list:
+            certificate_name = os.path.basename(certificate.get_full_file_name)
+            certificate_dir = os.path.abspath(certificate.get_full_file_name)
+            bcf = BankCertificateFile(certificate_dir, certificate_name)
+            if bcf.get_certificate_values() == 4:
+                if bcf.certificate_condition == 'cer':
+                    certificates_list.append(certificate.get_full_file_name)
+                else:
+                    pass
+            else:
+                pass
+        return certificates_list
--- classes/stored_file_classes.py
+++ classes/stored_file_classes.py
@@ -8,10 +8,6 @@ class StoredFile:
     Класс, предназначенный для хранения информации
     и статусе файла, включая данные о его блокировке.
     """
-    bank_code: str
-    snils: str
-    date_and_time: str
-    certificate_condition: str
 
     def __init__(self, file_path: str, file_name: str):
         """
@@ -29,6 +25,7 @@ class StoredFile:
         """
         return self.get_full_file_name
 
+
     @property
     def get_full_file_name(self):
         """
@@ -36,18 +33,6 @@ class StoredFile:
         :return: str: объединение пути и имени файла
         """
         return os.path.join(self.file_path, self.file_name)
-
-    @property
-    def get_splited_file_name(self):
-        splited_file_name = self.file_name.split('.')
-        if len(splited_file_name) == 4:
-            self.bank_code = splited_file_name[0]
-            self.snils = splited_file_name[1]
-            self.date_and_time = splited_file_name[2]
-            self.certificate_condition = splited_file_name[3]
-            return splited_file_name
-        else:
-            raise ValueError(f'{self.file_name} не является сертификатом!')
 
 
 class StoredFileContainer:
@@ -57,9 +42,6 @@ class StoredFileContainer:
     """
     path_class: typing.Type[StoredFile]
     path_class = StoredFile
-
-    CertificateFile: typing.Type[StoredFile]
-    CertificateFile = StoredFile
 
     def __init__(self, file_directory: str):
         """
@@ -72,7 +54,6 @@ class StoredFileContainer:
         self.certificates_list = []
         for files_instance in os.listdir(file_directory):
             self.files_list.append(self.path_class(file_directory, files_instance))
-            self.certificates_list.append(self.CertificateFile(file_directory, files_instance))
 
     def get_unlocked_files(self) -> list[str]:
         """
@@ -85,22 +66,3 @@ class StoredFileContainer:
             if not block_files_det_obj.file_is_locked(full_file_directory.get_full_file_name):
                 unlocked_files_list.append(full_file_directory.get_full_file_name)
         return unlocked_files_list
-
-    def get_deleted_certificates(self):
-        checked_certificates_list = []
-
-
-        pass
-
-    def get_certificates(self) -> 'typing.Iterable[StoredFile]':
-        checked_certificates_list = []
-        for certificate in self.certificates_list:
-            splited_file_name = certificate.get_splited_file_name
-            if len(splited_file_name) == 4:
-                self.bank_code = splited_file_name[0]
-                self.snils = splited_file_name[1]
-                self.date_and_time = splited_file_name[2]
-                self.certificate_condition = splited_file_name[3]
-                if self.certificate_condition == 'cer':
-                    checked_certificates_list.append(certificate)
-        return checked_certificates_list
