d057f8e6dadeff93ace19ee9b4d4cb6c21fb45be
Ксенофонтов Денис Евгеньевич <ksenofontov_d@tk.internal>
2023-04-19 13:38:48 +0300

    removed cwd and added correct path input PRAC-29

--- classes/exec_files.py
+++ classes/exec_files.py
@@ -1,19 +1,22 @@
 import subprocess
+import os
 
 
 class FileExec:
     def __init__(self, *args):
-        self.path = args[0]
-        self.params = args[1:]
+        """
+        :param args: передаваемые позиционные параметры
+        """
+        self.params = args
 
     def exec(self):
         """
-        Функция для выполнения файлов с передаваемыми параметрами в командной строке, возвращает кортеж из двух
-        элементов: кода выполнения команды и вывода
+        Функция для выполнения файлов с передаваемыми параметрами в командной строке
+        :return: возвращает кортеж из двух элементов: кода выполнения команды и вывода
         """
         file_list = []
         file_list.extend([value for value in self.params])
-        result = subprocess.run(file_list, stdout=subprocess.PIPE, shell=True, cwd=self.path,
+        result = subprocess.run(file_list, stdout=subprocess.PIPE, shell=True,
                                 stderr=subprocess.STDOUT)
         decoded_out = result.stdout.decode("oem")
         decoded_out.splitlines()
@@ -21,82 +24,116 @@ class FileExec:
 
 
 class EncryptExec:
-    def __init__(self, path: str, cert: str, msg_to_code: str, coded_msg: str):
-        self.file = 'cryptcp.exe'
+    def __init__(self, cert: str, msg_to_code: str, coded_msg: str):
+        """
+        :param cert: сертификат
+        :param msg_to_code: сообщение, которое нужно закодировать
+        :param coded_msg: закодированное сообщение
+        file: исполняемый файл
+        code: код для выполнения операции шифрования
+        param: параметры для выполнения файла
+        """
+        self.file = '../cryptcp_exec/cryptcp.exe'
+        self.file_with_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file)
         self.code = '-encr'
         self.param = r'-f'
         self.cert = cert
         self.msg_to_code = msg_to_code
         self.coded_msg = coded_msg
-        self.path = path
         self.output = self.exec_encr()
 
     def exec_encr(self):
         """
         Функция создает экземпляр класса FileExec с параметрами, которые выполняют шифрование файла
         с помощью программы cryptcp
+        :return: возвращает вывод подпроцесса
         """
-        encr_obj = FileExec(self.path, self.file, self.code, self.param, self.cert, self.msg_to_code, self.coded_msg)
+        encr_obj = FileExec(self.file_with_path, self.code, self.param, self.cert, self.msg_to_code, self.coded_msg)
         exec_out = encr_obj.exec()
         return exec_out
 
 
 class DecryptExec:
-    def __init__(self, path: str, cont: str, msg_to_decode: str, decoded_msg: str):
-        self.file = 'cryptcp.exe'
+    def __init__(self, cont: str, msg_to_decode: str, decoded_msg: str):
+        """
+        :param cont: параметр для поиска контейнера с заданным сертификатом
+        :param msg_to_decode: сообщение, которое нужно раскодировать
+        :param decoded_msg: раскодированное сообщение
+        file: исполняемый файл
+        code: код для выполнения операции дешифрования
+        param: параметры для выполнения файла
+        """
+        self.file = '../cryptcp_exec/cryptcp.exe'
+        self.file_with_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file)
         self.code = '-decr'
         self.param = '-dn'
         self.cont = cont
         self.msg_to_decode = msg_to_decode
         self.decoded_msg = decoded_msg
-        self.path = path
         self.output = self.exec_decr()
 
     def exec_decr(self):
         """
         Функция создает экземпляр класса FileExec с параметрами, которые выполняют дешифрование файла
         с помощью программы cryptcp
+        :return: возвращает вывод подпроцесса
         """
-        decr_obj = FileExec(self.path, self.file, self.code, self.param,
+        decr_obj = FileExec(self.file_with_path, self.code, self.param,
                             self.cont, self.msg_to_decode, self.decoded_msg)
         decr_out = decr_obj.exec()
         return decr_out
 
 
 class SetSignatureExec:
-    def __init__(self, path: str, cert: str, file_to_sign: str):
-        self.file = 'cryptcp.exe'
+    def __init__(self, cert: str, file_to_sign: str):
+        """
+        :param cert: параметр для поиска контейнера с заданным сертификатом
+        :param file_to_sign: файл, которйы нужно подписать
+        file: исполняемый файл
+        code: код для выполнения операции подписания
+        param: параметры для выполнения файла
+        """
+        self.file = '../cryptcp_exec/cryptcp.exe'
+        self.file_with_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file)
         self.code = '-sign'
         self.param = '-dn'
         self.cert = cert
         self.file_to_sign = file_to_sign
-        self.path = path
         self.output = self.exec_set_signature()
 
     def exec_set_signature(self):
         """
         Функция создает экземпляр класса FileExec с параметрами, которые создают ЭЦП файла
         с помощью программы cryptcp
+        :return: возвращает вывод подпроцесса
         """
-        set_obj = FileExec(self.path, self.file, self.code, self.param, self.cert, self.file_to_sign)
+        set_obj = FileExec(self.file_with_path, self.code, self.param, self.cert, self.file_to_sign)
         set_out = set_obj.exec()
         return set_out
 
 
 class UnsetSignatureExec:
-    def __init__(self, path: str, signed_file: str, unsigned_file: str):
-        self.file = 'cryptcp.exe'
+    def __init__(self, signed_file: str, unsigned_file: str):
+        """
+        :param signed_file: сообщение, с которого надо снять подпись
+        :param unsigned_file: файл, в который надо вывести текст исходного сообщения
+        file: исполняемый файл
+        code: код для выполнения операции снятия подписи
+        param: параметры для выполнения файла
+        """
+        self.file = '../cryptcp_exec/cryptcp.exe'
+        self.file_with_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.file)
         self.code = '-verify'
         self.signed_file = signed_file
         self.unsigned_file = unsigned_file
-        self.path = path
         self.output = self.exec_unset_signature()
 
     def exec_unset_signature(self):
         """
         Функция создает экземпляр класса FileExec с параметрами, которые снимают ЭЦП с файла
         с помощью программы cryptcp
+        :return: возвращает вывод подпроцесса
         """
-        unset_obj = FileExec(self.path, self.file, self.code, self.signed_file, self.unsigned_file)
+        unset_obj = FileExec(self.file_with_path, self.code, self.signed_file, self.unsigned_file)
         unset_out = unset_obj.exec()
         return unset_out
--- classes/exec_files_test.py
+++ classes/exec_files_test.py
@@ -5,22 +5,20 @@ import unittest
 class ExecFilesTestCase(unittest.TestCase):
 
     def test_encrypt(self):
-        certificate = r'cert_1.p7b'
-        message_to_code = r'test.txt'
-        coded_message = r'result.txt'
-        path = r'C:/Users/ksenofontov_d'
-        encr_exec = EncryptExec(path, certificate, message_to_code, coded_message)
+        certificate = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\cert_1.p7b'
+        message_to_code = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\test.txt'
+        coded_message = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\result.txt'
+        encr_exec = EncryptExec(certificate, message_to_code, coded_message)
         self.assertEqual(encr_exec.cert, certificate)
         self.assertEqual(encr_exec.msg_to_code, message_to_code)
         self.assertEqual(encr_exec.coded_msg, coded_message)
         print(f'Код выполнения:{encr_exec.output[0]},\nВывод:{encr_exec.output[1]}')
 
     def test_decrypt(self):
-        message_to_decode = r'to_decode.txt'
-        decoded_message = r'decoded.txt'
+        message_to_decode = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\to_decode.txt'
+        decoded_message = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\decoded.txt'
         container = r'CN=Denis'
-        path = r'C:/Users/ksenofontov_d'
-        decr_exec = DecryptExec(path, container, message_to_decode, decoded_message)
+        decr_exec = DecryptExec(container, message_to_decode, decoded_message)
         self.assertEqual(decr_exec.msg_to_decode, message_to_decode)
         self.assertEqual(decr_exec.decoded_msg, decoded_message)
         self.assertEqual(decr_exec.cont, container)
@@ -28,18 +26,16 @@ class ExecFilesTestCase(unittest.TestCase):
 
     def test_sign(self):
         container = r'CN=Denis'
-        sign_file = r'sign.txt'
-        path = r'C:/Users/ksenofontov_d'
-        sign_exec = SetSignatureExec(path, container, sign_file)
+        sign_file = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\sign.txt'
+        sign_exec = SetSignatureExec(container, sign_file)
         self.assertEqual(sign_exec.cert, container)
         self.assertEqual(sign_exec.file_to_sign, sign_file)
         print(f'Код выполнения:{sign_exec.output[0]},\nВывод:{sign_exec.output[1]}')
 
     def test_unsign(self):
-        signed_f = r'sign.txt.sig'
-        unsigned_f = r'sign_new.txt'
-        path = r'C:/Users/ksenofontov_d'
-        unsign_exec = UnsetSignatureExec(path, signed_f, unsigned_f)
+        signed_f = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\sign.txt.sig'
+        unsigned_f = r'C:\Users\ksenofontov_d\PycharmProjects\pythonProject\cryptcp_exec\sign_new.txt'
+        unsign_exec = UnsetSignatureExec(signed_f, unsigned_f)
         self.assertEqual(unsign_exec.signed_file, signed_f)
         self.assertEqual(unsign_exec.unsigned_file, unsigned_f)
         print(f'Код выполнения:{unsign_exec.output[0]},\nВывод:{unsign_exec.output[1]}')
