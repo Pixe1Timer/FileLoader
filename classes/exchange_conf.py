a90af9af6ceaf0f27056e8093cf7ff7e03c9bd68
ubuntu <denis.ksens@gmail.com>
2023-04-25 10:42:32 +0300

    implementation using os.path.exists and configs folder removed PRAC-39

--- classes/exchange_conf.py
+++ classes/exchange_conf.py
@@ -2,36 +2,22 @@ import os
 
 
 class ExchangeConf:
-    groups_path: str
-    users_path: str
-    procs_path: str
 
     def __init__(self, conf_path):
         self.conf_path = conf_path
-        self.paths_list = []
         self.names_confs = {'groups': '', 'users': '', 'procs': ''}
         self.scan_dir_for_file()
 
     def scan_dir_for_file(self):
-        for filename in os.listdir(self.conf_path):
-            if filename in self.names_confs:
-                self.names_confs[filename] = os.path.abspath(filename)
         for key in self.names_confs:
-            flag = True
-            if self.names_confs[key] == '':
-                print(f'Нет значения {key}')
-                flag = False
-        self.groups_path = self.names_confs['groups']
-        self.users_path = self.names_confs['users']
-        self.procs_path = self.names_confs['procs']
-        if not flag:
-            raise ValueError('Отсутствуют необходимые файлы!')
-        else:
-            print(
-                f'Файл с группами:{self.groups_path},'
-                f'\nФайл с пользователями:{self.users_path},'
-                f'\nФайл с процессами {self.procs_path}'
-            )
+            spec_path = os.path.join(self.conf_path, key)
+            if os.path.exists(spec_path):
+                self.names_confs[key] = spec_path
+            else:
+                raise ValueError(f'Нет значения {key}')
 
 
-a = ExchangeConf('/home/ubuntu/PycharmProjects/exchange_conf/exp')
+a = ExchangeConf('/home/ubuntu/PycharmProjects/exchange_conf/exp/configs/')
+print(a.names_confs['groups'])
+print(a.names_confs['users'])
+print(a.names_confs['procs'])
